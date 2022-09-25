7ba0af1f6d55c006507e49c21243169f3a9d4818
Alex Cattell <abcattell91@gmail.com>
2022-09-25 14:33:41 +0100

    Added list and wired up functions to detect when typed

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
 import Search_bar from './search_bar';
 import Gif from './Gif';
+import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "Qv7y2Tl4ki7Ru"
+    }
+  }
+
+  search = (query) => {
+    // TODO: APICall
+  }
+
+
   render () {
+    const gifs = [
+      { id: 'Qv7y2Tl4ki7Ru' },
+      { id: '9rtpurjbqiqZXbBBet' },
+    ]
+
     return (
       <div>
         <div className="left-scene">
           <Search_bar />
           <div className="selected-gif">
-          <Gif />
+          <Gif id={this.state.selectedGifId}/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs}/>
+        </div>
       </div>
     );
   }
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Gif extends Component {
   render() {
-    const src = "https://media0.giphy.com/media/1YeNJK6FptDdq1q59K/200.webp?cid=ecf05e47wz5o75xhr7z1qq42hfihean60myezoa4h2afgbkx&rid=200.webp&ct=g.gif";
+    const src = `https://media0.giphy.com/media/${this.props.id}/200.gif`;
     return (
       <img src={src} className="gif" />
     );
